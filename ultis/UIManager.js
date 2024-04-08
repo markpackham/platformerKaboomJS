56daf31e5e3fce82ef8d918bb6f48505dddd354f
mark <markpackham1@gmail.com>
2024-04-08 12:25:03 +0100

    Add blinking start message to screen
    https://youtu.be/wZpbTR7pYR0?si=FOVvdzTRApZdhB0O&t=2984

--- ultis/UIManager.js
+++ ultis/UIManager.js
@@ -13,6 +13,25 @@ class UIManager {
       // First param is the default state
       state("flash-up", ["flash-up", "flash-down"]),
     ]);
+
+    message.onStateEnter("flash-up", async () => {
+      // Tween allows smooth animation transition eg fade in
+      // await forces us to wait for the tween to finishing before moving on
+      await tween(message.opacity, 0, 0.5, (nextOpacityValue) => {
+        (message.opacity = nextOpacityValue),
+          // easings are the rate of change over time
+          easings.linear;
+      });
+      message.enterState("flash-down");
+    });
+
+    // Inverse logic of flash-up
+    message.onStateEnter("flash-down", async () => {
+      await tween(message.opacity, 1, 0.5, (nextOpacityValue) => {
+        (message.opacity = nextOpacityValue), easings.linear;
+      });
+      message.enterState("flash-up");
+    });
   }
 
   displayMainMenu() {
@@ -25,7 +44,12 @@ class UIManager {
       pos(center().x, center().y - 200),
       scale(8),
     ]);
+
+    this.displayBlinkingUIMessage(
+      "Press [ Enter ] to start",
+      vec2(center().x, center().y + 100)
+    );
   }
 }
 
-export const uiManager = new UIManager();
+export const uiManager = new UIManager("flash-up");
