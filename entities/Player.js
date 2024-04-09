0e731e5416e190a9f8d8080139d3c6978913257f
mark <markpackham1@gmail.com>
2024-04-09 12:34:00 +0100

    Add makePlayer() to Player class
    https://youtu.be/wZpbTR7pYR0?si=_od2G48KLEtwVtFb&t=7292

--- entities/Player.js
+++ entities/Player.js
@@ -9,12 +9,26 @@ export class Player {
     // isInTerminalScene checks if they completed the game
     isInTerminalScene
   ) {
-    this.posX = posX;
-    this.posY = posY;
-    this.speed = speed;
-    this.jumpForce = jumpForce;
-    this.nbLives = nbLives;
     this.currentLevelScene = currentLevelScene;
     this.isInTerminalScene = isInTerminalScene;
+    this.initialX = posX;
+    this.initialY = posY;
+    this.makePlayer();
+    this.speed = speed;
+    this.jumpForce = jumpForce;
+    this.lives = nbLives;
+    this.previousHeight = this.gameObj.pos.y;
+  }
+
+  makePlayer() {
+    this.gameObj = add([
+      sprite("player", { anim: "idle" }),
+      area({ shape: new Rect(vec2(0, 3), 8, 8) }),
+      anchor("center"),
+      pos(this.initialX, this.initialY),
+      scale(4),
+      body(),
+      "player",
+    ]);
   }
 }
