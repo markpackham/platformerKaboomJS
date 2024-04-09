5a8e6fca045708e42ded25476e9da71168c5f431
mark <markpackham1@gmail.com>
2024-04-09 10:03:46 +0100

    Add level1 folder
    https://youtu.be/wZpbTR7pYR0?si=nocMfeTEDa8oQTC_&t=4802

--- ultis/Level.js
+++ ultis/Level.js
@@ -1,6 +1,20 @@
 export class Level {
+  drawMapLayout(levelLayout, mappings) {
+    const layerSettings = {
+      tileWidth: 16,
+      tileHeight: 12,
+      tiles: mappings,
+    };
 
-    drawMapLayout(levelLayout, mappings)
+    this.map = [];
+    for (const layerLayout of levelLayout) {
+      this.map.push(addLevel(layerLayout, layerSettings));
+    }
+
+    for (const layer of this.map) {
+      layer.use(scale(4));
+    }
+  }
 
   drawBackground(bgSpriteName) {
     add([sprite(bgSpriteName), fixed(), scale(4)]);
