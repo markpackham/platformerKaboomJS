6aa764dd96312340aa59cba665af725b2f7b0fd1
mark <markpackham1@gmail.com>
2024-04-11 15:30:23 +0100

    Change Camera from class to a function
    https://youtu.be/wZpbTR7pYR0?si=K0mh267qwYT-YMny&t=8349

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import kaboom from "./libs/kaboom.mjs";
-import { uiManager } from "./utils/UIManager.js";
-import { load } from "./utils/loader.js";
 import { Level } from "./utils/Level.js";
 import { level1Layout, level1Mappings } from "./content/level1/level1Layout.js";
+import { load } from "./utils/loader.js";
 import { Player } from "./entities/Player.js";
-import { Camera } from "./utils/Camera.js";
+import { uiManager } from "./utils/UIManager.js";
+import attachCamera from "./utils/camera.js";
 
 kaboom({
   width: 1280,
@@ -27,6 +27,9 @@ const scenes = {
 
   // Levels
   1: () => {
+    // Gravity is built into Kaboom
+    setGravity(1400);
+
     const level1 = new Level();
     level1.drawBackground("forest-background");
     level1.drawMapLayout(level1Layout, level1Mappings);
@@ -35,8 +38,7 @@ const scenes = {
     const player = new Player(1500, 100, 400, 650, 3, 1, false);
 
     // Camera
-    const camera = new Camera();
-    camera.attach(player.gameObj, 0, -200);
+    attachCamera(player.gameObj, 0, 200);
 
     // Use wave animation
     level1.drawWaves("water", "wave");
--- utils/Camera.js
+++ utils/Camera.js
@@ -1,16 +1,7 @@
-export class Camera {
-  attachedObj = null;
-
-  attach(gameObj, offsetX = 0, offsetY = 0) {
-    this.attachedObj = gameObj;
-
-    // onUpdate provided by Kaboom & runs on every frame
-    onUpdate(() => {
-      // Camera position provided by Kaboom
-      camPos(
-        this.attachedObj.pos.x + offsetX,
-        this.attachedObj.pos.y + offsetY
-      );
-    });
-  }
+export default function attachCamera(attachedObj, offsetX, fixedY) {
+  // onUpdate provided by Kaboom & runs on every frame
+  onUpdate(() => {
+    // Camera position provided by Kaboom
+    camPos(attachedObj.pos.x + offsetX, fixedY);
+  });
 }
