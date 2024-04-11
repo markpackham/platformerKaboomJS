3282c72d89f68b8851eec9d567456c521209e497
mark <markpackham1@gmail.com>
2024-04-11 15:37:30 +0100

    Allow player to run left
    https://youtu.be/wZpbTR7pYR0?si=IzSWe5MBomelHBqZ&t=8649

--- entities/Player.js
+++ entities/Player.js
@@ -14,6 +14,8 @@ export class Player {
     this.initialX = posX;
     this.initialY = posY;
     this.makePlayer();
+    // Allow player movement
+    this.setPlayerControls();
     this.speed = speed;
     this.jumpForce = jumpForce;
     this.lives = nbLives;
@@ -23,6 +25,7 @@ export class Player {
 
   makePlayer() {
     this.gameObj = add([
+      // Default animation is idle
       sprite("player", { anim: "idle" }),
       area({ shape: new Rect(vec2(0, 3), 8, 8) }),
       anchor("center"),
@@ -31,5 +34,16 @@ export class Player {
       body(),
       "player",
     ]);
+  }
+
+  setPlayerControls() {
+    // Kamboo specific function, not standard JS
+    onKeyDown("left", () => {
+      if (this.gameObj.curAnim() !== "run") {
+        // Using Kaboom play "method" not to be confused with the Object independent
+        // play() function that is intended to play sounds while the Object method play() is for animation
+        this.gameObj.play("run");
+      }
+    });
   }
 }
