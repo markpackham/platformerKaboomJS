197035fbfb9ae24ce3ad373d9ad32a5fe0aeb47f
mark <markpackham1@gmail.com>
2024-04-18 09:31:15 +0100

    Add spider attributes
    https://youtu.be/wZpbTR7pYR0?si=ZiL7lc7HGnTcOJFy&t=16747

--- entities/Spiders.js
+++ entities/Spiders.js
@@ -1,8 +1,12 @@
 export class Spiders {
   // amplitudes concerns how far the spiders go back & forth
-  constructor(positions, amplitudes, velocities) {
+  // we use different spider types depending on the level
+  constructor(positions, amplitudes, velocities, type) {
     this.amplitudes = amplitudes;
     this.velocities = velocities;
     this.spiders = [];
+    for (const position of positions) {
+      this.spiders.push(add([sprite(`spider-${type}`, { anim: "crawl" })]));
+    }
   }
 }
